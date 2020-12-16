d6f9c3322c72705248d06c67304c54c2743b0559
Alessandro W <a.wollek@snapaddy.com>
2020-12-16 19:08:27 +0100

    feat: org-roam-server

--- config.el
+++ config.el
@@ -411,6 +411,19 @@ Is relative to `org-directory', unless it is absolute. Is used in Doom's default
 ;;     ))
 
 
+;; Org-Roam-Sever
+(setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 9090
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20)
+
 ;; EMACS APPLICATION FRAMEWORK
 ;; https://github.com/manateelazycat/emacs-application-framework#install
 ;; (use-package! eaf :load-path "~/projects/repositories/emacs-application-framework")

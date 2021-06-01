68eb2771f33697ae32f92b1f5812d42a2695d516
Alessandro W <alessandro.wollek@tum.de>
2021-06-01 18:24:16 +0200

    org-roam: template update

--- config/org-roam.el
+++ config/org-roam.el
@@ -7,17 +7,17 @@
 (setq org-roam-capture-templates
 '(("d" "default" plain
         (function org-roam-capture--get-point)
-        "%?\n* References"
+        "%?\n* Folgezettel\n\n* Related\n\n* References"
         :file-name "%<%Y%m%d%H%M%S>-${slug}"
-        :head "#+TITLE: \"\"${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_alias:\n#+roam_tags:\n\n"
+        :head "#+TITLE: ${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_alias:\n#+roam_tags: \"zettelkastenv2\"\n\n"
         :unnarrowed t)
         ))
 
 (setq org-roam-capture-ref-templates
 '(("r" "ref" plain (function org-roam-capture--get-point)
-        "%?\n* References\n- [[${ref}][Source]]"
+        "%?\n* Folgezettel\n\n* Related\n\n* References\n- [[${ref}][Source]]"
         :file-name "web/${slug}"
-        :head "#+TITLE: \"\"\n#+ROAM_ALIAS: \"${title}\"\n#+CREATED: %u\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_key: ${ref}\n#+roam_tags:\n\n"
+        :head "#+TITLE: \n#+ROAM_ALIAS: \"${title}\"\n#+CREATED: %u\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_key: ${ref}\n#+roam_tags: \"zettelkastenv2\" \"literature note\"\n\n"
         :unnarrowed t)
         ("t" "ref" plain (function org-roam-capture--get-point)
         "%?"

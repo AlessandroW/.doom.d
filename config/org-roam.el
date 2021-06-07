0cfa68788a9b30b4f4dbff478270fc6022c9ef03
Alessandro W <alessandro.wollek@tum.de>
2021-06-07 13:47:22 +0200

    feat: org roam server

--- config/org-roam.el
+++ config/org-roam.el
@@ -24,3 +24,16 @@
         :file-name "${ref}"
         :unnarrowed t)
         ))
+
+;; Org-Roam-Sever
+(setq org-roam-server-host "127.0.0.1"
+        org-roam-server-port 9090
+        org-roam-server-authenticate nil
+        org-roam-server-export-inline-images t
+        org-roam-server-serve-files nil
+        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+        org-roam-server-network-poll t
+        org-roam-server-network-arrows nil
+        org-roam-server-network-label-truncate t
+        org-roam-server-network-label-truncate-length 60
+        org-roam-server-network-label-wrap-length 20)

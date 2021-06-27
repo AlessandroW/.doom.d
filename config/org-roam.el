4f6f6fea79dc8ace9b75c36f4997f1743d2ebb0d
Alessandro W <alessandro.wollek@tum.de>
2021-06-27 22:20:02 +0200

    fix: load org capture templates before org roam

--- config/org-roam.el
+++ config/org-roam.el
@@ -2,26 +2,25 @@
 
 ;; ORG ROAM
 (setq org-roam-directory "~/org/org-roam")
-(after! org-roam
-  (setq org-roam-capture-templates
-        '(("d" "default" plain
-           (function org-roam-capture--get-point)
-           "%?\n* Folgezettel\n\n* Related\n\n* References"
-           :file-name "%<%Y%m%d%H%M%S>-${slug}"
-           :head "#+TITLE: ${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_alias:\n#+roam_tags: \"zettelkastenv2\"\n\n"
-           :unnarrowed t)
-          ))
-  (setq org-roam-capture-ref-templates
-        '(("r" "ref" plain (function org-roam-capture--get-point)
-           "%?\n* Folgezettel\n\n* Related\n\n* References\n- [[${ref}][Source]]"
-           :file-name "web/${slug}"
-           :head "#+TITLE: \n#+ROAM_ALIAS: \"${title}\"\n#+CREATED: %u\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_key: ${ref}\n#+roam_tags: \"zettelkastenv2\" \"literature note\"\n\n"
-           :unnarrowed t)
-          ("t" "ref" plain (function org-roam-capture--get-point)
-           "%?"
-           :file-name "${ref}"
-           :unnarrowed t)
-          )))
+(setq org-roam-capture-templates
+'(("d" "default" plain
+        (function org-roam-capture--get-point)
+        "%?\n* Folgezettel\n\n* Related\n\n* References"
+        :file-name "%<%Y%m%d%H%M%S>-${slug}"
+        :head "#+TITLE: ${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_alias:\n#+roam_tags: \"zettelkastenv2\"\n\n"
+        :unnarrowed t)
+        ))
+(setq org-roam-capture-ref-templates
+'(("r" "ref" plain (function org-roam-capture--get-point)
+        "%?\n* Folgezettel\n\n* Related\n\n* References\n- [[${ref}][Source]]"
+        :file-name "web/${slug}"
+        :head "#+TITLE: \n#+ROAM_ALIAS: \"${title}\"\n#+CREATED: %u\n#+LAST_MODIFIED: %U\n#+setupfile:~/org/org-roam/hugo_setup.org\n#+roam_key: ${ref}\n#+roam_tags: \"zettelkastenv2\" \"literature note\"\n\n"
+        :unnarrowed t)
+        ("t" "ref" plain (function org-roam-capture--get-point)
+        "%?"
+        :file-name "${ref}"
+        :unnarrowed t)
+        ))
 ;; Org-Roam-Sever
 (use-package! org-roam-server
   :after (org-roam-server)

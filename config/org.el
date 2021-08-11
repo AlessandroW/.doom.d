4cf586f8d257b602c6d1ebb2f1162271cd82a4d9
Alessandro W <alessandro.wollek@tum.de>
2021-08-11 13:15:57 +0200

    feat: org-mode highlight bold and italic text

--- config/org.el
+++ config/org.el
@@ -8,6 +8,13 @@
   ;; Use a separate inbox to not mess up SyncThing and Orgzly
   (setq! +org-capture-todo-file "~/org/desktop_inbox.org")
 
+  (add-to-list 'org-emphasis-alist
+             '("_" (:foreground "#ff6655" :weight italic)))
+  (add-to-list 'org-emphasis-alist
+             '("*" (:foreground "#282c34" :background "#ECBE7B" :weight bold)))
+  (setq org-hide-emphasis-markers t)
+
+
   (setq! org-agenda-files (-concat
                            '("~/org/next_action.org" "~/org/inbox.org" "~/org/desktop_inbox.org")
                            (directory-files-recursively "~/org/projects" org-agenda-file-regexp))))

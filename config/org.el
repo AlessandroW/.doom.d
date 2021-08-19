73b44877286f13a7fde87d12c8a6a70a368ff598
Alessandro W <alessandro.wollek@tum.de>
2021-08-19 10:11:18 +0200

    fix: italic face

--- config/org.el
+++ config/org.el
@@ -9,13 +9,17 @@
   (setq! +org-capture-todo-file "~/org/desktop_inbox.org")
 
   (add-to-list 'org-emphasis-alist
-             '("/" (:foreground "#ff6655" :weight italic)))
+             '("/" (:foreground "#ff6655" :slant italic)))
   (add-to-list 'org-emphasis-alist
              '("*" (:foreground "#282c34" :background "#ECBE7B" :weight bold)))
 
   (setq! org-agenda-files (-concat
                            '("~/org/next_action.org" "~/org/inbox.org" "~/org/desktop_inbox.org")
                            (directory-files-recursively "~/org/projects" org-agenda-file-regexp))))
+(defun no-line-numbers ()
+  (setq-local display-line-numbers nil)
+)
+(add-hook 'org-mode-hook 'no-line-numbers)
 
 (use-package! org-fragtog
   :after org

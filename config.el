8c8412bed6b1eda39a986f8eaa44144d284c15f4
Alessandro Wollek <a@wollek.dev>
2022-10-18 18:30:46 +0200

    use pdf-tools for latex

--- config.el
+++ config.el
@@ -124,3 +124,14 @@
                                                 :todo "TODO")
                                          ))
   :config (org-super-agenda-mode))
+
+(after! latex
+  ;; Use pdf-tools to open PDF files
+  ;; https://emacs.stackexchange.com/a/19475
+  (setq TeX-view-program-selection '((output-pdf "PDF Tools"))
+        TeX-source-correlate-start-server t)
+
+  ;; Update PDF buffers after successful LaTeX runs
+  (add-hook 'TeX-after-compilation-finished-functions
+            #'TeX-revert-document-buffer)
+  )
